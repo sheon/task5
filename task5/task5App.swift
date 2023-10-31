bdc5049b169382365a5cd86c0ca1331fa016a374
Ehsan Abdolmajidi <ehsan.msc87@gmail.com>
2023-10-31 23:08:17 +0100

    Initial Commit

---
+++ task5/task5App.swift
@@ -0,0 +1,17 @@
+//
+//  task5App.swift
+//  task5
+//
+//  Created by Ehsan Abdolmajidi on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct task5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
